8e5c8cabc581a364a98df3c42d101e14eac3ba17
@cheeks cloud <agneskaswii700@gmail.com>
2022-04-09 08:27:02 +0300

    Validating the form

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit,Output,EventEmitter} from '@angular/core';
-
+import { Quotes } from 'src/app/quotes'
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
@@ -7,7 +7,7 @@ import { Component, OnInit,Output,EventEmitter} from '@angular/core';
 })
 export class QuoteFormComponent implements OnInit {
 
-  
+  newQuote = new Quotes(0,"","","","",0,0);
   constructor() { }
 
   ngOnInit(): void {
